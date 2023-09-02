74e5eee04cf28d532fc2cac0a3713f6559f2be0c
breemark <britomaximiliano83@gmail.com>
2023-09-02 11:29:34 +0700

    Navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,34 @@
 <x-guest-layout>
 
+    <!-- Navbar -->
+    <nav class="relative select-none bg-teal-300 lg:flex lg:items-stretch w-full">
+        <div class="flex flex-no-shrink items-stretch h-12">
+            <a href="#" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">
+                Home
+            </a>
+            <a href="#" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">
+                Blog
+            </a>
+        <!--
+
+            <button class="block lg:hidden cursor-pointer ml-auto relative w-12 h-12 p-4">
+            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+            </button>
+        </div>
+
+        <div class="lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
+            <div class="lg:flex lg:items-stretch lg:justify-end ml-auto">
+            <a href="#" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Item 1</a>
+            <a href="#" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Item 2</a>
+            <a href="#" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Item 3</a>
+            </div>
+        </div>
+        -->
+    </nav>
+
     <!-- Hero Section -->
-    <div class="flex flex-col bg-teal-50">
+    <div class="flex flex-col bg-teal-50 pt-4">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
@@ -12,7 +39,7 @@
                 </p>
 
             </div>
-            <div class="text-center lg:col-span-5 lg:flex">
+            <div class="hidden sm:block lg:col-span-5 lg:flex">
                 <img class="rounded-lg w-90 drop-shadow-md" src="{{url('/images/hero-profile.jpg')}}" alt="profile picture">
             </div>                
         </div>
